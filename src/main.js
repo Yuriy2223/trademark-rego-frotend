b5e8d93954048e2fc3bf777fb2f16ae8f21d7d95
Yuriy <>
2025-02-13 15:49:51 +0200

    1

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './js/header';
 import './js/video';
 import './js/product-card';
 import './js/certificates';
+
+// import './scss/style.scss';
+// import './js/header.js';
+// import './js/video.js';
+// import './js/product-card.js';
+// import './js/certificates.js';
