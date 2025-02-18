03a95f678aa12669045d9fea5cb73105b19cf279
Yuriy <>
2025-02-18 16:36:44 +0200

    Update nav-menu.js

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,9 +1,28 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const currentPath = window.location.pathname;
+
+//   function setActiveClass(links, activeClass) {
+//     links.forEach(link => {
+//       if (link.getAttribute('href') === currentPath) {
+//         link.classList.add(activeClass);
+//       }
+//     });
+//   }
+
+//   const menuLinks = document.querySelectorAll('.menu-link');
+//   const burgerMenuLinks = document.querySelectorAll('.burger-menu-link');
+
+//   setActiveClass(menuLinks, 'menu-link-active');
+//   setActiveClass(burgerMenuLinks, 'burger-menu-link-active');
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
-  const currentPath = window.location.pathname;
+  const currentPath = window.location.pathname.split('/').pop();
 
   function setActiveClass(links, activeClass) {
     links.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
+      const linkPath = link.getAttribute('href').split('/').pop();
+      if (linkPath === currentPath) {
         link.classList.add(activeClass);
       }
     });
